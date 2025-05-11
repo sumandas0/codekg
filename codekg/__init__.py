2aa668349508707d26e7fd93e48f8fae81995e06
Suman Das <59254445+sumandas0@users.noreply.github.com>
2025-05-11 12:48:32 +0530

    feat: reset

--- codekg/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-CodeKG - An extensible knowledge graph framework for code.
-"""
-
-__version__ = "0.1.0" 
--- codekg/graph/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Graph database integration for CodeKG.
-"""
-
-from .memgraph_client import MemgraphClient
-
-__all__ = [
-    "MemgraphClient"
-] 
